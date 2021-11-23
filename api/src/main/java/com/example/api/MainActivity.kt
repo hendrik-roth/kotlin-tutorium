3986ee4d101d13b92d996b898fb6b0d71fc4b489
marcelgnamm <marcel.gnamm@gmx.de>
2021-11-23 15:59:52 +0100

    :beers: Added import statements

--- api/src/main/java/com/example/api/MainActivity.kt
+++ api/src/main/java/com/example/api/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.api
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.http.cio.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
