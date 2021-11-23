e39c6e2f7a8d5d5cd93d4eea7827c1ef1bc94410
Hendrik Roth <hroth@stud.hs-heilbronn.de>
2021-11-23 17:20:31 +0100

    :beers: added corona api

--- api/src/main/java/com/example/api/MainActivity.kt
+++ api/src/main/java/com/example/api/MainActivity.kt
@@ -1,26 +1,41 @@
 package com.example.api
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import io.ktor.client.*
-import io.ktor.client.request.*
-import io.ktor.http.cio.*
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 
 
 class MainActivity : AppCompatActivity() {
+    private var mTextViewResult: TextView? = null
+    private var mQueue: RequestQueue? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        runBlocking {
-            launch {
-                val client = HttpClient()
-                val response: Response = client.get("https://api.corona-zahlen.org/germany")
-                Log.d("response", response.statusText.toString())
-            }
-        }
+        mTextViewResult = findViewById(R.id.text_view_result)
+        val buttonParse = findViewById<Button>(R.id.button_parse)
+
+        mQueue = Volley.newRequestQueue(this)
+        buttonParse.setOnClickListener { jsonParse() }
+    }
+
+    private fun jsonParse() {
+        val url = "https://api.corona-zahlen.org/germany"
+        val request = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+
+                val cases = response.get("cases")
+                val format = java.text.NumberFormat.getIntegerInstance().format(cases.toString().toInt()).replace(",", ".")
+
+                mTextViewResult?.text = format
+
+            }, { error -> error.printStackTrace() })
+        mQueue?.add(request)
     }
 }
