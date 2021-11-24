a49471000e3165e826a95eb44ec9daa47a69c777
Hendrik Roth <hroth@stud.hs-heilbronn.de>
2021-11-24 14:44:50 +0100

    update api app

--- api/src/main/java/com/example/api/MainActivity.kt
+++ api/src/main/java/com/example/api/MainActivity.kt
@@ -12,19 +12,40 @@ import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     private var mTextViewResult: TextView? = null
+    private var textViewDeaths: TextView? = null
     private var mQueue: RequestQueue? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mTextViewResult = findViewById(R.id.text_view_result)
-        val buttonParse = findViewById<Button>(R.id.button_parse)
 
+        textViewDeaths = findViewById(R.id.tv_deaths)
+        val buttonParse = findViewById<Button>(R.id.button_parse)
         mQueue = Volley.newRequestQueue(this)
-        buttonParse.setOnClickListener { jsonParse() }
+
+        showDeaths()
+        mQueue?.start()
+
+        buttonParse.setOnClickListener { getCases() }
     }
 
-    private fun jsonParse() {
+    private fun showDeaths() {
+        val url = "https://api.corona-zahlen.org/germany"
+        val request = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+
+                val deaths = response.get("deaths")
+                val format = java.text.NumberFormat.getIntegerInstance().format(deaths.toString().toInt()).replace(",", ".")
+                val resultText = "Tode: $format"
+                textViewDeaths?.text = resultText
+
+            }, { error -> error.printStackTrace() })
+        mQueue?.add(request)
+    }
+
+    private fun getCases() {
         val url = "https://api.corona-zahlen.org/germany"
         val request = JsonObjectRequest(
             Request.Method.GET, url, null,
@@ -32,8 +53,8 @@ class MainActivity : AppCompatActivity() {
 
                 val cases = response.get("cases")
                 val format = java.text.NumberFormat.getIntegerInstance().format(cases.toString().toInt()).replace(",", ".")
-
-                mTextViewResult?.text = format
+                val result = "Anzahl Fälle insgesamt: $format"
+                mTextViewResult?.text = result
 
             }, { error -> error.printStackTrace() })
         mQueue?.add(request)
